abcd7d069f1907f3694cb7f2d24f462cb2edb21f
Arthur Mateu <arthurmateu@gmail.com>
2024-01-27 22:51:11 +0100

    improved readability exercise

---
+++ foundational-csharp-ms/improving-readability/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Prints a reversed string and counts how many times a hard-coded character repeats
+*/
+
+string initialMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = initialMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message) if (letter == 'o') letterCount++;
+
+string finalMessage = new String(message);
+
+Console.WriteLine(finalMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
