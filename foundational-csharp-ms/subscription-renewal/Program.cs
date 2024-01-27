bd253b4b85959f527ecbef388d4c7373843a592e
Arthur Mateu <arthurmateu@gmail.com>
2024-01-27 21:06:42 +0100

    Subscription renewal exercise

---
+++ foundational-csharp-ms/subscription-renewal/Program.cs
@@ -0,0 +1,28 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if ((daysUntilExpiration < 10) && (daysUntilExpiration > 5))
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else if (daysUntilExpiration >= 1)
+{
+    discountPercentage += 10;
+    
+    if (daysUntilExpiration == 1)
+    {
+        discountPercentage += 10;
+        Console.WriteLine("Your subscription expires within a day!");
+    }
+    else
+    {
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days");
+    }
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
