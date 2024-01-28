962228c5cd32b71e326f2b820e54de064abb9733
Arthur Mateu <arthurmateu@gmail.com>
2024-01-28 14:41:51 +0100

    conditional operators exercises

---
+++ foundational-csharp-ms/conditional-operators/Program.cs
@@ -0,0 +1,25 @@
+﻿// Heads or tails
+using System;
+
+Random random = new Random();
+string heads = "Heads", tails = "Tails";
+
+Console.WriteLine($"{(random.Next(2) == 1 ? heads : tails)}");
+
+
+// Initialize permission and level values
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin")) {
+    if (level > 55) {
+        Console.WriteLine("Welcome, Super Admin user.");
+    } else {
+        Console.WriteLine("Welcome, Admin user");
+    }
+} else if (permission.Contains("Manager") && level >= 20) {
+    Console.WriteLine("Contact an Admin for access.");
+} else {
+    Console.WriteLine("You do not have sufficient privileges");
+}
