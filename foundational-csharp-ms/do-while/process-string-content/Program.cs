0316f9a4e6afe5d0de98dc9125106890a9a9e44e
Arthur Mateu <arthurmateu@gmail.com>
2024-01-28 19:48:55 +0100

    process string content exercise

---
+++ foundational-csharp-ms/do-while/process-string-content/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    Expected output:
+        I like pizza
+        I like roast chicken
+        I like salad
+        I like all three of the menu choices
+
+    Expected output didn't happen (should also find a better way to trim the first space)
+*/
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+foreach(string sentence in myStrings) {
+    foreach(char word in sentence) {
+        if (word == '.') {
+            Console.Write('\n'); 
+            continue;
+        }
+        Console.Write(word);
+    }
+    Console.WriteLine();
+}
