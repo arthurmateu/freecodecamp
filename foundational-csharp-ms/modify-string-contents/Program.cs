61c274b4840a50e5fb1842bb0d749a67b88798c5
Arthur Mateu <arthurmateu@gmail.com>
2024-01-30 23:38:37 +0100

    modify string contents exercise

---
+++ foundational-csharp-ms/modify-string-contents/Program.cs
@@ -0,0 +1,29 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string open = "<span>";
+const string close = "</span>";
+
+int quantityStart = input.IndexOf(open) + open.Length;
+int quantityEnd= input.IndexOf(close);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
