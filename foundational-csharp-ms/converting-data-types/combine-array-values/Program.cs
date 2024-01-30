8e6934bfdcb3fbe6e4cac14387805be5f6ad8c82
Arthur Mateu <arthurmateu@gmail.com>
2024-01-30 16:00:35 +0100

    combine array values exercise

---
+++ foundational-csharp-ms/converting-data-types/combine-array-values/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string sentence = "";
+double elementIsNumber = 0; // Has to initialize first, for it to be able to be fed into TryParse
+double result = 0;
+
+foreach(string element in values){
+    if (double.TryParse(element, out elementIsNumber))
+        result += elementIsNumber;
+    else sentence += element;
+};
+
+Console.WriteLine($"Sentence: {sentence}");
+Console.WriteLine($"Result: {result}");
