8758f1e4544b488569c2176f689690462efc18bb
Arthur Mateu <arthurmateu@gmail.com>
2024-01-30 20:10:16 +0100

    form letter exercise

---
+++ foundational-csharp-ms/format-alphanumeric-data/form-letter/Program.cs
@@ -0,0 +1,26 @@
+﻿string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine($"Currently, you own {currentShares} shares at a return of {currentReturn:P2}.");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+comparisonMessage += currentProduct.PadRight(32) + $"{currentReturn:P2}".PadRight(16) + $"{currentProfit:C2}\n";
+comparisonMessage += newProduct.PadRight(32) + $"{newReturn:P2}".PadRight(16) + $"{newProfit:C2}\n";
+
+Console.WriteLine(comparisonMessage);
