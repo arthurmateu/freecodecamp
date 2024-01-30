6b49b06b60ec65c12786181de4855a85e313944a
Arthur Mateu <arthurmateu@gmail.com>
2024-01-30 18:47:10 +0100

    sentence reverse exercise

---
+++ foundational-csharp-ms/array-operations/sentence-reverse/Program.cs
@@ -0,0 +1,13 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] pangramArray = pangram.Split(' ');
+string[] newPangram = new string[pangramArray.Length];
+for (int i = 0; i < pangramArray.Length; i++){
+    char[] letters = pangramArray[i].ToCharArray();
+    Array.Reverse(letters);
+    newPangram[i] = new string(letters);
+}
+string result = String.Join(" ", newPangram);
+
+Console.WriteLine($"Original string: {pangram}");
+Console.WriteLine($"Reversed string: {result}");
