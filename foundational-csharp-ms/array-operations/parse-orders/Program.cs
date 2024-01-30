1a8edb6fbcd4e869e4bf0a46af7d22e4d44441cb
Arthur Mateu <arthurmateu@gmail.com>
2024-01-30 18:59:55 +0100

    parse orders exercise

---
+++ foundational-csharp-ms/array-operations/parse-orders/Program.cs
@@ -0,0 +1,15 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+
+string[] orderArray = orderStream.Split(',');
+Array.Sort(orderArray);
+
+foreach(string order in orderArray){
+    Console.Write(order);
+
+    // This is honestly pretty lazy. Better implementation would be to make counters and make sure the order has 1 letter followed by 3 digits
+    // Update: I just saw the answer, apparently this is acceptable (lmao)
+    if (order.Length != 4 )
+        Console.Write("\t- Error");
+
+    Console.Write('\n');
+}
