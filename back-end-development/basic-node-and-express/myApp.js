64374ee3bdebe0d95321428a7e7c710c77b3149a
Arthur Mateu <arthurmateu@gmail.com>
2024-02-08 21:58:11 +0100

    Implemented a root-level request logger middleware and slight visual clean-up

--- back-end-development/basic-node-and-express/myApp.js
+++ back-end-development/basic-node-and-express/myApp.js
@@ -2,18 +2,23 @@ let express = require('express');
 let app = express();
 require('dotenv').config()
 
-
 console.log("Hello World");
-
 absolutePath = __dirname + "/views/index.html";
 
+// Middleware has to come before everything else
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+  });
+
+
 app.get("/", (req, res) => {
-    // res.send("Hello Express")
     res.sendFile(absolutePath);
     }
 );
-
-app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
     (process.env.MESSAGE_STYLE === "uppercase")
