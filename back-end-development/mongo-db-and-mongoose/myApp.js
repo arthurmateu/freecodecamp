d5b6ea64fb8973c69fb26eeb8695ab1afc6e3a41
Arthur Mateu <arthurmateu@gmail.com>
2024-02-11 16:06:41 +0100

    Chaining search queries

--- back-end-development/mongo-db-and-mongoose/myApp.js
+++ back-end-development/mongo-db-and-mongoose/myApp.js
@@ -96,7 +96,10 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch}).sort('name').limit(2).select("-age").exec(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 /** **Well Done !!**
