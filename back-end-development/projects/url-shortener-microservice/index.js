540b15a61403490bcdf8fa89c4b87733e5708525
Arthur Mateu <arthurmateu@gmail.com>
2024-02-23 14:34:58 +0100

    URL Shortener Microservice project

--- back-end-development/projects/url-shortener-microservice/index.js
+++ back-end-development/projects/url-shortener-microservice/index.js
@@ -2,23 +2,58 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+// Dependencies
+const dns = require('dns')
+const urlparser = require('url')
+//Database connection
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.DB_URL)
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// PROJECT: Posting URLs will save them into a database, and return a json with the corresponding number to access them later, using /api/shorturl/:number with the GET http method
+app.post('/api/shorturl', function (req, res) {
+    const url = req.body.url
+    dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+        if (!address) {
+            res.json({ error: "invalid url" })
+        } else {
+            const urlCount = await urls.countDocuments({})
+            const urlDoc = {
+                url: url,
+                short_url: urlCount
+            }
+
+            await urls.insertOne(urlDoc)
+            res.json({
+                original_url: url,
+                short_url: urlCount
+            })
+        }
+    })
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.get("/api/shorturl/:short_url", async (req, res) => {
+    const shorturl = req.params.short_url
+    const urlDoc = await urls.findOne({ short_url: +shorturl })
+    res.redirect(urlDoc.url)
+})
+
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
 });
